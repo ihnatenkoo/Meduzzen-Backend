01e0ff9cfab465815c8e06b9936ee91781d0ac12
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2023-10-23 18:02:53 +0300

    Formated code

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -3,10 +3,10 @@ import { AppService } from './app.service';
 
 @Controller()
 export class AppController {
-	constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) {}
 
-	@Get()
-	getHealthCheck() {
-		return this.appService.getHealthCheck();
-	}
+  @Get()
+  getHealthCheck() {
+    return this.appService.getHealthCheck();
+  }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,7 @@ import { HttpStatus, Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-	getHealthCheck() {
-		return { status_code: HttpStatus.OK, detail: 'ok', result: 'working' };
-	}
+  getHealthCheck() {
+    return { status_code: HttpStatus.OK, detail: 'ok', result: 'working' };
+  }
 }
