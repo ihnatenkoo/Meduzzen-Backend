97d3866eb4a680c3a2f3316e6b7d9499b7bbdab0
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2023-10-26 09:32:22 +0300

    Add db port to env

--- db/data-source.ts
+++ db/data-source.ts
@@ -5,7 +5,7 @@ import 'dotenv/config';
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
   host: process.env.DB_HOST ?? 'localhost',
-  port: 5432,
+  port: Number(process.env.DB_PORT),
   database: process.env.POSTGRES_DB,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
