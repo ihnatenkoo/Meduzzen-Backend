4a7f71d03b4a7174400f34e6080155d80f53fdc8
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2023-10-26 09:25:43 +0300

    Add host and port env vars to dataSourceOptions

--- db/data-source.ts
+++ db/data-source.ts
@@ -1,9 +1,10 @@
 import { DataSource, DataSourceOptions } from 'typeorm';
+import 'dotenv/config';
 
 export const dataSourceOptions: DataSourceOptions = {
   type: 'postgres',
-  host: 'postgres',
-  port: 5432,
+  host: process.env.DB_HOST ?? 'localhost',
+  port: Number(process.env.DB_PORT),
   database: process.env.POSTGRES_DB,
   username: process.env.POSTGRES_USER,
   password: process.env.POSTGRES_PASSWORD,
