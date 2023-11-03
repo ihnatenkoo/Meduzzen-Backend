2bafda67aaa36e36a69812365d524ff4a8a58624
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2023-11-03 19:52:44 +0200

    Add swagger in quiz controller

--- src/quiz/quiz.controller.ts
+++ src/quiz/quiz.controller.ts
@@ -8,21 +8,23 @@ import {
   UseGuards,
   UsePipes,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { User } from 'src/decorators/user.decorator';
 import { AuthGuard } from 'src/guards/auth.guard';
 import { DtoValidationPipe } from 'src/pipes/dtoValidation.pipe';
 import { IdValidationPipe } from 'src/pipes/idValidationPipe';
-import { CreateQuizDto } from './dto/createQuiz.dto';
+import { CreateQuizDto, UpdateQuizDto } from './dto/createQuiz.dto';
 import { IMessage } from 'src/types';
 import { QuizEntity } from './quiz.entity';
 import { QuizService } from './quiz.service';
 
+@ApiBearerAuth()
 @ApiTags('quiz')
 @Controller('quiz')
 export class QuizController {
   constructor(private readonly quizService: QuizService) {}
 
+  @ApiOperation({ summary: 'Create quiz in Company' })
   @Post('create/:companyId')
   @UseGuards(AuthGuard)
   @UsePipes(new DtoValidationPipe())
@@ -34,17 +36,22 @@ export class QuizController {
     return this.quizService.createQuiz(userId, companyId, createQuizDto);
   }
 
+  @ApiOperation({ summary: 'Update quiz by ID' })
+  @ApiBody({
+    type: UpdateQuizDto,
+  })
   @Patch(':id')
   @UseGuards(AuthGuard)
   @UsePipes(new DtoValidationPipe())
   async updateQuiz(
     @User('id') userId: number,
     @Param('id', IdValidationPipe) quizId: number,
-    @Body() updateQuizDto: Partial<CreateQuizDto>,
+    @Body() updateQuizDto: UpdateQuizDto,
   ): Promise<{ quiz: QuizEntity }> {
     return this.quizService.updateQuiz(userId, quizId, updateQuizDto);
   }
 
+  @ApiOperation({ summary: 'Delete quiz by ID' })
   @Delete(':id')
   @UseGuards(AuthGuard)
   async deleteQuiz(
