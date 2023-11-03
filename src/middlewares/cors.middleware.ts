a3b9e7c81b8f14807c9594d3bf475b1f60de6cc8
Dmytro Ihnatenko <ihnatenko.dev@gmail.com>
2023-11-03 15:54:13 +0200

    Fix cors middleware, add condition for swagger

--- src/middlewares/cors.middleware.ts
+++ src/middlewares/cors.middleware.ts
@@ -10,9 +10,13 @@ import { Request, Response, NextFunction } from 'express';
 export class CorsMiddleware implements NestMiddleware {
   use(req: Request, res: Response, next: NextFunction) {
     const reqOrigin = req.header('Origin');
+    const referer = req.header('Referer');
 
-    if (process.env.ALLOWED_ORIGINS.includes(reqOrigin)) {
-      res.header('Access-Control-Allow-Origin', reqOrigin);
+    if (
+      process.env.ALLOWED_ORIGINS.includes(reqOrigin) ||
+      referer === process.env.SWAGGER_API
+    ) {
+      res.header('Access-Control-Allow-Origin', reqOrigin || referer);
       res.header('Access-Control-Allow-Headers', 'content-type');
       res.header(
         'Access-Control-Allow-Methods',
